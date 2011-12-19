8d43e810a31bf0a84dc7c543b937b9715e8ed909
sunhao.java@gmail.com <sunhao.java@gmail.com@21732885-1028-19ca-3633-2b6d5caa21fe>
2011-12-19 14:29:12 +0000

    工具类

--- trunk/ my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/utils/WebOutput.java
+++ trunk/ my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/utils/WebOutput.java
@@ -2,9 +2,12 @@ package com.message.utils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.io.Writer;
+import java.util.Locale;
 import java.util.Map;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.io.IOUtils;
 
 @SuppressWarnings({"unchecked","unused"})
-public class WebOutput {
+public class WebOutput implements HttpServletResponse {
 	private HttpServletResponse response;
 	private HttpServletRequest request;
 	private static Map _defaultMimeTypes;
@@ -105,5 +108,191 @@ public class WebOutput {
 	
 	public void flush() throws IOException{
 		this.response.flushBuffer();
+	}
+
+	@Override
+	public void addCookie(Cookie arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addDateHeader(String arg0, long arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addHeader(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addIntHeader(String arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean containsHeader(String arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String encodeRedirectURL(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String encodeRedirectUrl(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String encodeURL(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String encodeUrl(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void sendError(int arg0) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendError(int arg0, String arg1) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendRedirect(String arg0) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setDateHeader(String arg0, long arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setHeader(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setIntHeader(String arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setStatus(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setStatus(int arg0, String arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void flushBuffer() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getBufferSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getCharacterEncoding() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getContentType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Locale getLocale() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ServletOutputStream getOutputStream() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PrintWriter getWriter() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isCommitted() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resetBuffer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setBufferSize(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setCharacterEncoding(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setContentLength(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setLocale(Locale arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }

83e2f2bef6bf71000c8a86cccde2efb995ff4ae3
sunhao.java@gmail.com <sunhao.java@gmail.com@21732885-1028-19ca-3633-2b6d5caa21fe>
2011-12-19 14:28:19 +0000

    管理员管理模块基本写好

---  my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/main/admin/web/AdminController.java
+++  my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/main/admin/web/AdminController.java
@@ -1,5 +1,49 @@
 package com.message.main.admin.web;
 
-public class AdminController {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+import com.message.main.admin.pojo.Admin;
+import com.message.main.admin.service.AdminService;
+import com.message.utils.WebInput;
+import com.message.utils.WebOutput;
+
+public class AdminController extends MultiActionController {
+	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+	
+	private AdminService adminService;
+
+	public void setAdminService(AdminService adminService) {
+		this.adminService = adminService;
+	}
+	
+	/**
+	 * 进入管理员列表页面
+	 * @param in
+	 * @param out
+	 * @param admin
+	 * @return
+	 */
+	public ModelAndView inListAdminJsp(HttpServletRequest request, HttpServletResponse response){
+		Map<String, Object> params = new HashMap<String, Object>();
+		List<Admin> admins = null;
+		try {
+			admins = this.adminService.getAdmins();
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e.getMessage(), e);
+		}
+		params.put("admins", admins);
+		return new ModelAndView("message.list.admin", params);
+	}
 
 }

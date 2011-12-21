8c49fa01b689dc7e964935c3654b1aefcb804631
sunhao.java@gmail.com <sunhao.java@gmail.com@21732885-1028-19ca-3633-2b6d5caa21fe>
2011-12-21 10:46:39 +0000

    是否登录的拦截器，未配置，也未完成

---
+++  my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/filter/IfLoginFilter.java
@@ -0,0 +1,36 @@
+package com.message.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class IfLoginFilter implements Filter {
+
+	public void destroy() {
+		
+	}
+
+	public void doFilter(ServletRequest req, ServletResponse res,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+		HttpSession session = request.getSession(true);
+		String url = request.getServletPath();
+		
+		
+		
+	}
+
+	public void init(FilterConfig config) throws ServletException {
+		
+	}
+
+}

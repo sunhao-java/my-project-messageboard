d1bffc3753e6495e7725a07dc4b7776851265c66
sunhao.java@gmail.com <sunhao.java@gmail.com@21732885-1028-19ca-3633-2b6d5caa21fe>
2011-12-27 13:17:59 +0000

    去除不必要的封装，修改weboutput

---  my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/utils/WebOutput.java
+++  my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/utils/WebOutput.java
@@ -1,26 +1,23 @@
 package com.message.utils;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
-import java.io.Writer;
-import java.util.Locale;
+import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.io.IOUtils;
-
-@SuppressWarnings({"unchecked","unused"})
-public class WebOutput implements HttpServletResponse {
+@SuppressWarnings("unchecked")
+public class WebOutput {
 	private HttpServletResponse response;
 	private HttpServletRequest request;
 	private static Map _defaultMimeTypes;
+	private static Map _defaultCharacter;
 	
 	static{
+		_defaultMimeTypes = new HashMap();
 		_defaultMimeTypes.put(".avi", "video/x-msvideo");
 		_defaultMimeTypes.put(".bmp", "image/bmp");
 		_defaultMimeTypes.put(".class", "application/octet-stream");
@@ -44,17 +41,34 @@ public class WebOutput implements HttpServletResponse {
 		_defaultMimeTypes.put(".wav", "audio/x-wav");
 		_defaultMimeTypes.put(".xml", "text/xml");
 		_defaultMimeTypes.put(".zip", "application/zip");
+		
+		_defaultCharacter = new HashMap();
+		_defaultCharacter.put("gbk", "GBK");
+		_defaultCharacter.put("utf8", "UTF-8");
+		_defaultCharacter.put("gb2312", "GB2312");
 	}
 	
-	public WebOutput(HttpServletRequest request, HttpServletResponse response){
+	public WebOutput(HttpServletRequest request, HttpServletResponse response) {
 		this.request = request;
 		this.response = response;
 	}
 	
-	public HttpServletResponse getResponse(){
-		return this.response;
+	public HttpServletResponse getResponse() {
+		return response;
 	}
-	
+
+	public void setResponse(HttpServletResponse response) {
+		this.response = response;
+	}
+
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
 	public void setCookie(String name, String value, int maxAge){
 		Cookie c = new Cookie(name, value);
 		if(maxAge > 0){
@@ -77,222 +91,21 @@ public class WebOutput implements HttpServletResponse {
 	}
 	
 	public void print(String content) throws IOException{
-		Writer out = this.response.getWriter();
+		PrintWriter out = this.response.getWriter();
 		out.write(content);
+		out.close();
 	}
 	
-	public void copy(InputStream in) throws IOException{
-		IOUtils.copy(in, this.response.getOutputStream());
-	}
-	
-	public void copy(byte[] data) throws IOException{
-		IOUtils.write(data, this.response.getOutputStream());
-	}
-	
-	public void redirect(String url) throws IOException{
+	public void sendRedirect(String url) throws IOException{
 		this.response.sendRedirect(url);
 	}
 	
-	public void toJson(Object obj) throws IOException{
-		this.response.setHeader("Pragma", "No-cache");
-		this.response.setHeader("Cache-Control", "no-cache");
-		this.response.setDateHeader("Expires", 0L);
-		
-		this.print(JsonUtils.toString(obj));
-	}
-	
-	public void toJson(String rootName, Object obj) throws IOException {
-		setContentType("application/x-javascript", "UTF-8");
-		print(JsonUtils.toString(rootName, obj));
+	public void toJson(Object obj) throws Exception{
+		this.response.setContentType(_defaultMimeTypes.get(".xml") + ";charset=" + _defaultCharacter.get("utf8"));
+		this.print(obj.toString());
 	}
 	
 	public void flush() throws IOException{
 		this.response.flushBuffer();
-	}
-
-	@Override
-	public void addCookie(Cookie arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addDateHeader(String arg0, long arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addHeader(String arg0, String arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addIntHeader(String arg0, int arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean containsHeader(String arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public String encodeRedirectURL(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String encodeRedirectUrl(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String encodeURL(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String encodeUrl(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void sendError(int arg0) throws IOException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void sendError(int arg0, String arg1) throws IOException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void sendRedirect(String arg0) throws IOException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setDateHeader(String arg0, long arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setHeader(String arg0, String arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setIntHeader(String arg0, int arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setStatus(int arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setStatus(int arg0, String arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void flushBuffer() throws IOException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public int getBufferSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public String getCharacterEncoding() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getContentType() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Locale getLocale() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ServletOutputStream getOutputStream() throws IOException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public PrintWriter getWriter() throws IOException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean isCommitted() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void reset() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void resetBuffer() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setBufferSize(int arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setCharacterEncoding(String arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setContentLength(int arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setLocale(Locale arg0) {
-		// TODO Auto-generated method stub
-		
 	}
 }

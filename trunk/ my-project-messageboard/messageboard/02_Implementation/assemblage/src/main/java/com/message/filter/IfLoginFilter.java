248b28aec512f973d2a4128c3f833a054bff52a4
sunhao.java@gmail.com <sunhao.java@gmail.com@21732885-1028-19ca-3633-2b6d5caa21fe>
2012-01-07 14:58:54 +0000

    未登录的拦截器

--- trunk/ my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/filter/IfLoginFilter.java
+++ trunk/ my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/filter/IfLoginFilter.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.message.utils.resource.ResourceType;
+
 /**
  * 判断是否登录的filter，拦截一些未登录的非法URL
  * @author sunhao(sunhao.java@gmail.com)
@@ -22,7 +24,6 @@ public class IfLoginFilter implements Filter {
 		
 	}
 
-	@SuppressWarnings("unused")
 	public void doFilter(ServletRequest req, ServletResponse res,
 			FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) req;
@@ -38,17 +39,15 @@ public class IfLoginFilter implements Filter {
 			System.out.println("method=[" + request.getMethod() + "]");
 			System.out.println("end-------------------");
 		}
-		/*User user = (User) session.getAttribute(ResourceType.LOGIN_USER_KEY_IN_SESSION);
-		if(!"/index.jsp".equals(url)){
-			if(user != null){
-				response.sendRedirect(request.getContextPath() + ResourceType.LOGIN_PAGE_URL);
+		if(url.indexOf(".do") != -1 && !url.equals("/user/inLogin.do") && !url.equals("/user/login.do")){
+			if(session.getAttribute(ResourceType.LOGIN_USER_KEY_IN_SESSION) == null){
+				response.sendRedirect(request.getContextPath() + "/user/inLogin.do");
 			} else {
 				chain.doFilter(req, res);
 			}
 		} else {
 			chain.doFilter(req, res);
-		}*/
-		chain.doFilter(req, res);
+		}
 	}
 
 	public void init(FilterConfig config) throws ServletException {

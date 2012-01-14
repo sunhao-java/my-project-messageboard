33f75852901f552576d47610732a1c8ba6efa4ad
sunhao.java@gmail.com <sunhao.java@gmail.com@21732885-1028-19ca-3633-2b6d5caa21fe>
2012-01-14 21:41:23 +0000

    国际化资源工具类的开发

---
+++  my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/utils/ApplicationContextUtil.java
@@ -0,0 +1,30 @@
+package com.message.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * 普通Java类获取Spring的ApplicationContext<br/>
+ * 在web应用中，某些情况我们需要在类中来获得spring所管理的bean</br>
+ * 可以使用ApplicationContext context = new ClassPathXmlApplicationContext("classpath:**.xml")来获取<br/>
+ * 但是每次都得创建，比较繁琐也会耗资源<br/>
+ * 所以就有了下面这个工具类<br/>
+ * 创建一个类并让其实现org.springframework.context.ApplicationContextAware接口来让Spring在启动的时候为我们注入ApplicationContext对象.
+ * 
+ * @author sunhao(sunhao.java@gmail.com)
+ */
+public class ApplicationContextUtil implements ApplicationContextAware {
+	private static ApplicationContext applicationContext;//声明一个静态变量保存   
+
+	@SuppressWarnings("static-access")
+	public void setApplicationContext(ApplicationContext applicationContext)
+			throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+	
+	public static ApplicationContext getContext(){  
+		return applicationContext;  
+	}   
+
+}

378f2ffdd33494d99de4a158e2603f9be1a783f1
sunhao.java@gmail.com <sunhao.java@gmail.com@21732885-1028-19ca-3633-2b6d5caa21fe>
2012-01-14 00:03:00 +0000

    添加公共的删除单条和批量删除的JS方法

---  my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/webapp/js/base/commfunction.js
+++  my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/webapp/js/base/commfunction.js
@@ -42,6 +42,104 @@ function linkFun(targetUrl){
 	parent.frames['main'].location.href = targetUrl;
 }
 
+/**
+ * 自动提示组件用
+ * @param {Object} color
+ * @param {Object} width
+ * @param {Object} wrap
+ */
 function showUser(color,width,wrap){
 	$(".showUser span").colorTip({color:color,width:width,wrap:wrap});
+}
+
+/**
+ * 全部选中的方法
+ * @param {Object} selallid
+ * @param {Object} listcheckboxname
+ */
+function selectAll(selallid, listcheckboxname){
+    var selAll = document.getElementById(selallid);
+    var PIds = document.getElementsByName(listcheckboxname);
+    var PIdLength = PIds.length;
+    if(selAll.checked){
+        for(var i=0;i<PIdLength;i++){
+            PIds[i].checked = true;
+        }
+    }else{
+        for(var i=0;i<PIdLength;i++){
+            PIds[i].checked = false;
+        }
+    }
+}
+
+/**
+ * 批量删除的方法
+ * @param {Object} boxId		ckechBox框的ID
+ * @param {Object} requestURL	请求URL
+ * @param {Object} responseURL	响应URL
+ */
+function deleteMore(boxId, requestURL, responseURL){
+	var dataString = '';
+	var boxes = document.getElementsByName(boxId);
+	var boxesLength = boxes.length;
+	for(var i = 0; i < boxesLength; i++){
+		if(boxes[i].checked){
+			dataString += boxes[i].value + ',';
+		}
+	}
+	
+	if(dataString == '' || dataString.length == 0){
+		YAHOO.app.dialog.pop({'dialogHead':'提示','cancelButton':'false','alertMsg':'请选择需要删除的行！'});
+		return;
+	} else {
+		YAHOO.app.dialog.pop({'dialogHead':'提示','alertMsg':'你确定要删除所选中的项吗？',
+			'confirmFunction':function(){
+				if(requestURL.indexOf('?') == -1){
+					requestURL += '?';
+				}
+				requestURL += 'pkIds=' + dataString;
+				$C.asyncRequest('POST', requestURL, {
+					success : function(o){
+						var _e = eval("(" + o.responseText + ")");
+						if(_e.status == '1'){
+							YAHOO.app.dialog.pop({'dialogHead':'提示','cancelButton':'false','alertMsg':'删除成功！',
+								'confirmFunction':function(){
+									window.location.href = responseURL;
+								}});
+						} else if(_e.status == '0'){
+							YAHOO.app.dialog.pop({'dialogHead':'提示','cancelButton':'false','alertMsg':'删除失败！'});
+						}
+					},
+					failure : function(o){
+						YAHOO.app.dialog.pop({'dialogHead':'提示','cancelButton':'false','alertMsg':'错误代码:' + o.status});
+					}
+				});
+			}});
+	}
+}
+
+/**
+ * 删除单条
+ * @param {Object} requestURL	请求URL
+ * @param {Object} responseURL	响应URL
+ */
+function deleteOne(requestURL, responseURL){
+	YAHOO.app.dialog.pop({'dialogHead':'提示','alertMsg':'你确定要要删除此条留言吗？','confirmFunction':function(){
+		$C.asyncRequest('POST', requestURL, {
+			success : function(o){
+				var _e = eval("(" + o.responseText + ")");
+				if(_e.status == '1'){
+					YAHOO.app.dialog.pop({'dialogHead':'提示','cancelButton':'false','alertMsg':'删除成功！',
+						'confirmFunction':function(){
+							window.location.href = responseURL;
+						}});
+				} else {
+					YAHOO.app.dialog.pop({'dialogHead':'提示','cancelButton':'false','alertMsg':'删除失败！'});
+				}
+			}, 
+			failure : function(o){
+				YAHOO.app.dialog.pop({'dialogHead':'提示','cancelButton':'false','alertMsg':'错误代码:' + o.status});
+			}
+		});
+	}});
 }

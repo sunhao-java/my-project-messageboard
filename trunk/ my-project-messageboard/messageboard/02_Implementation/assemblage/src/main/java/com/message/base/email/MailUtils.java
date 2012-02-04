748a19fd46f6e1343cee6c53170cff358b2fe4d4
sunhao.java@gmail.com <sunhao.java@gmail.com@21732885-1028-19ca-3633-2b6d5caa21fe>
2012-02-04 21:28:25 +0000

    解决国际化资源文件类和邮件服务器配置类在spring中的加载顺序

--- trunk/ my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/base/email/MailUtils.java
+++ trunk/ my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/base/email/MailUtils.java
@@ -1,5 +1,6 @@
 package com.message.base.email;
 
+import java.util.List;
 import java.util.Properties;
 
 import javax.mail.Address;
@@ -14,12 +15,25 @@ import javax.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.message.base.MessageUtils;
+import com.message.utils.MD5Utils;
+import com.message.utils.StringUtils;
+
 /**
  * 邮件发送工具类
  * @author sunhao(sunhao.java@gmail.com)
  */
 public class MailUtils {
 	private static final Logger LOGGER = LoggerFactory.getLogger(MailUtils.class);
+	
+	private final String SYS_SENDER_USERNAME = MessageUtils.getMessage("sys.sender.username", "sunhao0550@163.com");
+	private final String SYS_SENDER_PASSWORD = MessageUtils.getMessage("sys.sender.password", "sunhao1314520");
+	private final String EMAIL_AT = "@";
+	private final String EMAIL_DAO = ".";
+	private final String CONFIRM_TIP = MessageUtils.getMessage("confirm.tip", "请点击下面的链接验证用户！");
+	private final String CONFIRM_TIP_AGAIN = MessageUtils.getMessage("confirm.tip.again", "如果没有成功，请复制下面链接到浏览器地址栏！");
+	private final String URL_CONFIRM = MessageUtils.getMessage("user.confirm", "http://sunhao.wiscom.com.cn:8089/message/user/emailConfirm.do?");
+	private final String MAIL_CONFIRM_TITLE = MessageUtils.getMessage("mail.confirm.title", "邮件验证");
 	
 	private static MailUtils instance = new MailUtils();
 	
@@ -107,5 +121,52 @@ public class MailUtils {
 		}
 		
 		return message;
+	}
+	
+	/**
+	 * 组装要发送邮件的内容
+	 * @param pkId
+	 * @param username
+	 * @return
+	 */
+	public String makeMailContent(Long pkId, String username){
+		StringBuffer sb = new StringBuffer();
+		String username_md5 = MD5Utils.MD5Encode(username);
+		sb.append(CONFIRM_TIP).append("<br/>");
+		sb.append("<a href='").append(URL_CONFIRM).append("usernameMD5Code=").append(username_md5).append("&userid=").append(pkId);
+		sb.append("'>").append(URL_CONFIRM).append("usernameMD5Code=").append(username_md5).append("&userid=").append(pkId).append("</a>");
+		sb.append("<br/>");
+		sb.append(CONFIRM_TIP_AGAIN).append("<br/>");
+		sb.append(URL_CONFIRM).append("usernameMD5Code=").append(username_md5).append("&userid=").append(pkId);
+		
+		return sb.toString();
+	}
+	
+	/**
+	 * 获得SendMailProperties
+	 * 
+	 * @param mailProperties
+	 * @return
+	 */
+	public SendMailProperties getSendProperties(List<SendMailProperties> mailProperties){
+		String mailServiceDomainName = StringUtils.getBetweenTwoLetters(SYS_SENDER_USERNAME, EMAIL_AT, EMAIL_DAO);
+		for(SendMailProperties properties : mailProperties) {
+			if(properties.getName().equals(mailServiceDomainName)){
+				properties.setSenderUsername(SYS_SENDER_USERNAME);
+				properties.setSenderPassword(SYS_SENDER_PASSWORD);
+				
+				return properties;
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * 获取邮件主题
+	 * @return
+	 */
+	public String getMailTitle(){
+		return MAIL_CONFIRM_TITLE;
 	}
 }
---
+++ trunk/ my-project-messageboard/messageboard/02_Implementation/assemblage/src/test/java/com/message/utils/message/MessageUtilsTest.java
@@ -0,0 +1,24 @@
+package com.message.utils.message;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.message.base.MessageUtils;
+
+import junit.framework.TestCase;
+@SuppressWarnings("unused")
+public class MessageUtilsTest extends TestCase {
+	
+	@Before
+	public void before(){
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+	}
+	
+	@Test
+	public void testGetMessage(){
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+		
+		System.out.println(MessageUtils.getMessage("user.confirm", "1111"));
+	}
+}

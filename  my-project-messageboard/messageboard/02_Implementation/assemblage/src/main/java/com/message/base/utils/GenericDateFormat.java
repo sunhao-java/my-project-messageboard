1dd98b6331f283cf5788048c8233af3effee5109
sunhao.java@gmail.com <sunhao.java@gmail.com@21732885-1028-19ca-3633-2b6d5caa21fe>
2012-02-06 14:54:15 +0000

    整理

---
+++  my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/base/utils/GenericDateFormat.java
@@ -0,0 +1,66 @@
+package com.message.base.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 对date类型的操作
+ * @author sunhao(sunhao.java@gmail.com)
+ */
+public class GenericDateFormat {
+	SimpleDateFormat dateFormat;
+	
+	public GenericDateFormat(String pattern){
+		this.dateFormat = new SimpleDateFormat(pattern);
+	}
+	
+	public String format(Date value){
+		return this.dateFormat.format(value);
+	}
+	
+	public static String format(Date value, String pattern){
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+		return sdf.format(value);
+	}
+	
+	public Date parse(String text) throws ParseException{
+		Date result = null;
+		try{
+			result = this.dateFormat.parse(text);
+		} catch(ParseException e){
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public static Date parse(String text, String pattern) throws ParseException{
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+		return sdf.parse(text);
+	}
+	
+	public static Date strToDate(String strDate, String format){
+		SimpleDateFormat sdf = new SimpleDateFormat(format);
+		Date date = null;
+		try {
+			date = sdf.parse(strDate);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		return date;
+	}
+	
+	public static Date strToDateTime(String strDateTime, String fromat){
+		SimpleDateFormat dateTimeFormat = new SimpleDateFormat(fromat);
+		Date dateTime = null;
+		try {
+			dateTime = dateTimeFormat.parse(strDateTime);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		return dateTime;
+	}
+	
+}

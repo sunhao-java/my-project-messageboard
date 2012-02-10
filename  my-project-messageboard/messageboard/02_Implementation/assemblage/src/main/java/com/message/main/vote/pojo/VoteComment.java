aed6f2689c2ee6421acf3b639761838e2dd80286
sunhao.java@gmail.com <sunhao.java@gmail.com@21732885-1028-19ca-3633-2b6d5caa21fe>
2012-02-10 15:50:41 +0000

    投票POJO类，新建，未写字段

---
+++  my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/main/vote/pojo/VoteComment.java
@@ -0,0 +1,11 @@
+package com.message.main.vote.pojo;
+
+/**
+ *
+ * @author sunhao(sunhao.java@gmail.com)
+ * @version V1.0
+ * @createTime 2012-2-10 下午11:50:12
+ */
+public class VoteComment {
+
+}
---
+++  my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/main/vote/pojo/VoteOption.java
@@ -0,0 +1,11 @@
+package com.message.main.vote.pojo;
+
+/**
+ *
+ * @author sunhao(sunhao.java@gmail.com)
+ * @version V1.0
+ * @createTime 2012-2-10 下午11:24:16
+ */
+public class VoteOption {
+
+}

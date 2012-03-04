02d7c828faa8d693c11ccf4e354c05905f2e1730
sunhao.java@gmail.com <sunhao.java@gmail.com@21732885-1028-19ca-3633-2b6d5caa21fe>
2012-03-04 02:38:39 +0000

    菜单管理

---
+++ trunk/ my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/base/spring/AuthContext.java
@@ -0,0 +1,22 @@
+package com.message.base.spring;
+
+import com.message.main.user.pojo.User;
+
+/**
+ * 存放当前登录者
+ * 
+ * @author sunhao(sunhao.java@gmail.com)
+ * @version V1.0
+ * @createTime 2012-3-4 上午09:08:31
+ */
+public class AuthContext {
+	private User loginUser;
+
+	public User getLoginUser() {
+		return loginUser;
+	}
+
+	public void setLoginUser(User loginUser) {
+		this.loginUser = loginUser;
+	}
+}

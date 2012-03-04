f8d0a402f6f708cecc84b49943525a1032522573
sunhao.java@gmail.com <sunhao.java@gmail.com@21732885-1028-19ca-3633-2b6d5caa21fe>
2012-03-04 02:38:39 +0000

    菜单管理

---
+++  my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/base/spring/AuthContextHelper.java
@@ -0,0 +1,22 @@
+package com.message.base.spring;
+
+/**
+ * 往ThreadLocal放入取出当前登录者
+ * 
+ * @author sunhao(sunhao.java@gmail.com)
+ * @version V1.0
+ * @createTime 2012-3-4 上午09:09:29
+ */
+@SuppressWarnings({"unchecked", "rawtypes"})
+public class AuthContextHelper {
+    private static ThreadLocal authContextHolder = new ThreadLocal();
+
+    public static AuthContext getAuthContext() {
+        return (AuthContext) authContextHolder.get();
+    }
+
+    public static void setAuthContext(AuthContext authContext) {
+        authContextHolder.set(authContext);
+    }
+
+}

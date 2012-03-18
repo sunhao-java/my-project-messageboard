d10d16ef0ceb79fef0e0b02b26ab5f44a20d0a6d
sunhao.java@gmail.com <sunhao.java@gmail.com@21732885-1028-19ca-3633-2b6d5caa21fe>
2012-03-18 13:25:08 +0000

    上传组件

---
+++  my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/base/upload/impl/GenericUploadServiceImpl.java
@@ -0,0 +1,89 @@
+package com.message.base.upload.impl;
+
+import com.message.base.Constants;
+import com.message.base.upload.GenericUploadService;
+import com.message.base.utils.FileUtils;
+import com.message.base.utils.ImageUtils;
+import com.message.base.utils.MD5Utils;
+import com.message.base.utils.StringUtils;
+import com.message.resource.ResourceType;
+import org.aspectj.apache.bcel.classfile.Constant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * 上传文件的通用类的实现
+ *
+ * @author sunhao(sunhao.java@gmail.com)
+ * @version V1.0
+ * @createTime 12-3-18 上午11:39
+ */
+public class GenericUploadServiceImpl implements GenericUploadService {
+    private static final Logger logger = LoggerFactory.getLogger(GenericUploadServiceImpl.class);
+
+    public void uploads(MultipartRequest request) throws Exception {
+        Iterator it = request.getFileNames();
+
+        while(it.hasNext()){
+            String key = (String) it.next();
+            if(logger.isDebugEnabled()){
+                logger.debug("the filaName key is '{}'!", key);
+            }
+            MultipartFile file = request.getFile(key);
+            this.upload(file);
+        }
+        
+    }
+
+    public void upload(MultipartFile file) throws Exception {
+        String fileName = file.getOriginalFilename();
+        if(logger.isDebugEnabled()){
+            logger.debug("the fileName is '{}'", fileName);
+        }
+        String suffix = fileName.substring(fileName.indexOf("."));
+        if(StringUtils.isEmpty(suffix)){
+            logger.error("can not find suffix from given fileName '{}'!", fileName);
+        }
+
+        fileName = MD5Utils.MD5Encode(fileName) + suffix;
+
+        String path = Constants.DEFAULT_UPLOAD_PAYH;
+
+        makeImageBySize(path, file, fileName);
+    }
+
+    /**
+     * 保存字节码文件到硬盘
+     *
+     * @param savePath      保存的路径
+     * @param file          文件
+     * @param fileName      文件名（算好的，MD5加密的文件名）
+     * @throws Exception
+     */
+    private void makeImageBySize(String savePath, MultipartFile file, String fileName) throws Exception{
+        String fileRealPath = StringUtils.EMPTY;
+        byte[] fileByte = file.getBytes();
+
+        Date now = new Date();
+        String datePath = new SimpleDateFormat("/yyyy/MM/dd/").format(now);
+
+        File uploadFile = new File(savePath + datePath);
+        
+        if(!uploadFile.exists()){
+            org.apache.commons.io.FileUtils.forceMkdir(uploadFile);
+        }
+
+        fileRealPath = savePath + "/" + datePath + "/" + fileName;
+
+        FileUtils.createFile(fileRealPath, fileByte);
+    }
+}

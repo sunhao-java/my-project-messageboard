a38ceb1d538242eaf8532f2e6245341dc33807e9
sunhao.java@gmail.com <sunhao.java@gmail.com@21732885-1028-19ca-3633-2b6d5caa21fe>
2012-06-17 05:37:50 +0000

    为图片加水印

--- trunk/ my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/base/utils/ImageUtils.java
+++ trunk/ my-project-messageboard/messageboard/02_Implementation/assemblage/src/main/java/com/message/base/utils/ImageUtils.java
@@ -1,12 +1,19 @@
 package com.message.base.utils;
 
-import java.awt.Image;
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGEncodeParam;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
+import org.apache.commons.lang.*;
+
+import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileOutputStream;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
 
 /**
  * 图片处理工具类(需要整理)
@@ -136,5 +143,154 @@ public class ImageUtils {
 		
 		return new int[]{w, h};
 	}
+
+    /**
+     * 给图片添加文字水印
+     *
+     * @param filePath 			需要添加水印的图片的路径
+     * @param markContent 		水印的文字
+     * @param markContentColor 	水印文字的颜色
+     * @param fontSize			字体大小
+     * @param location			位置(1、左上角；2、右上角；3、右下角；4、左下角；5、中间)
+     * @return 布尔类型
+     * @throws Exception
+     */
+    public boolean createStringMark(String filePath, String markContent, Color markContentColor, int fontSize,
+                                    int location) throws Exception {
+        ImageIcon imgIcon = new ImageIcon(filePath);
+        Image theImg = imgIcon.getImage();
+        int width = theImg.getWidth(null);
+        int height = theImg.getHeight(null);
+        BufferedImage bimage = new BufferedImage(width, height,
+                BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = bimage.createGraphics();
+        g.setColor(markContentColor);
+        g.setBackground(Color.white);
+        g.drawImage(theImg, 0, 0, null);
+        g.setFont(new Font("楷体", Font.PLAIN, fontSize)); // 字体、字型、字号
+
+        int length = 0;
+        char[] tmp = markContent.toCharArray();
+        String en = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`~!@#$%^&*()-_+=[]{};:'\",.<>?0123456789";
+        String flag_cn = "·～！￥×（）——『』【】；：‘“，。？《》";
+        for(char t : tmp){
+            if (org.apache.commons.lang.StringUtils.contains(en, t)) {
+                length += fontSize / 2;
+            } else if (org.apache.commons.lang.StringUtils.contains(flag_cn, t)) {
+                length += fontSize;
+            } else {
+                length += fontSize;
+            }
+        }
+        length += 10;
+
+        int w,h;
+        switch(location){
+            case 1:
+                w = 5;
+                h = fontSize;
+                break;
+            case 2:
+                w = width - length;
+                h = fontSize;
+                break;
+            case 3:
+                w = width - length;
+                h = height - 10;
+                break;
+            case 4:
+                w = 5;
+                h = height - 10;
+                break;
+            case 5:
+                w = (width - length) / 2;
+                h = (height - fontSize) / 2;
+                break;
+            default:
+                throw new Exception("没有位置！");
+        }
+        g.drawString(markContent, w, h); // 画文字
+        g.dispose();
+        try {
+            FileOutputStream out = new FileOutputStream(filePath); // 先用一个特定的输出文件名
+            JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
+            JPEGEncodeParam param = encoder.getDefaultJPEGEncodeParam(bimage);
+            param.setQuality(7, true);
+            encoder.encode(bimage, param);
+            out.close();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 给图片添加图像水印
+     *
+     * @param filePath 		需要添加水印的图片的路径
+     * @param markPath		水印图片路径
+     * @param location		位置(1、左上角；2、右上角；3、右下角；4、左下角；5、中间)
+     * @return 布尔类型
+     * @throws Exception
+     */
+    public boolean createWaterMarkDemo(String filePath, String markPath, int location) throws Exception {
+        // 要处理的原始图片
+        ImageIcon icoInput = new ImageIcon(filePath);
+        Image imgInput = icoInput.getImage();
+        int width = imgInput.getWidth(null);
+        int height = imgInput.getHeight(null);
+        BufferedImage buffInput = new BufferedImage(width, height,
+                BufferedImage.TYPE_INT_RGB);
+        // 要添加上来的水印
+        ImageIcon icoADD = new ImageIcon(markPath);
+        Image imgADD = icoADD.getImage();
+        int w = imgADD.getWidth(null);
+        int h = imgADD.getHeight(null);
+        // 绘图
+        Graphics2D g = buffInput.createGraphics();
+        g.drawImage(imgInput, 0, 0, null);
+        
+        int x,y;
+        switch(location){
+            case 1:
+                x = 10;
+                y = 10;
+                break;
+            case 2:
+                x = width - w - 10;
+                y = 10;
+                break;
+            case 3:
+                x = width - w - 10;
+                y = height - h - 10;
+                break;
+            case 4:
+                x = 10;
+                y = height - h - 10;
+                break;
+            case 5:
+                x = (width - w) / 2;
+                y = (height - h) / 2;
+                break;
+            default:
+                throw new Exception("没有位置！");
+        }
+        // 下面代码的前面五个参数：图片，x坐标，y坐标,图片宽度,图片高度
+        g.drawImage(imgADD, x, y, w, h, null);
+
+        g.dispose();
+        try {
+            FileOutputStream out = new FileOutputStream(filePath);
+            JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
+            JPEGEncodeParam param = encoder
+                    .getDefaultJPEGEncodeParam(buffInput);
+            param.setQuality(7, true);
+            encoder.encode(buffInput, param);
+            out.close();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
   
 }  
